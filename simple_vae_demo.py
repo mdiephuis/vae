23e64e888519d6883350f14997f813631ebaf912
Maurits Diephuis <mdiephuis@gmail.com>
2018-12-02 17:56:06 +0100

    redid model example, added parameters

--- simple_vae_demo.py
+++ simple_vae_demo.py
@@ -1,25 +1,18 @@
-"""
-    Python (C)VAE implementations
-    Maurits Diephuis
-"""
-
 from __future__ import print_function
 import numpy as np
 import argparse
 import torch
-import torch.nn as nn
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.optim import Adam
-from torch.nn import functional as F
-from torchvision import datasets, transforms
 from torchvision.utils import save_image
-from torch.utils.data import DataLoader
 
 from vae_models import CVAE
 
 from nn_helpers.losses import loss_bce_kld, EarlyStopping
-from nn_helpers.utils import one_hot_np, init_weights
+from nn_helpers.utils import init_weights, one_hot
+from nn_helpers.utils import type_tfloat
 from nn_helpers.visdom_grapher import VisdomGrapher
+from nn_helpers.data import Loader
 
 
 parser = argparse.ArgumentParser(description='VAE example')
@@ -32,12 +25,26 @@ parser.add_argument('--uid', type=str, default='VAE',
 
 parser.add_argument('--conditional', action='store_true', default=True,
                     help='Enable CVAE')
+parser.add_argument('--latent-size', type=int, default=20, metavar='N',
+                    help='VAE latent size (default: 20')
+
+parser.add_argument('--encoder-size', type=int, default=200, metavar='N',
+                    help='VAE encoder size (default: 200')
+
+# data loader parameters
+parser.add_argument('--dataset-name', type=str, default=None,
+                    help='Name of dataset (default: none')
+parser.add_argument('--data-dir', type=str, default=None,
+                    help='directory to load data from (default: none')
+parser.add_argument('--download-data', action='store_true', default=False,
+                    help='Automatically download dataset (default: false)')
+parser.add_argument('--batch-size', type=int, default=23, metavar='N',
+                    help='input training batch-size')
+
 
 # Optimizer
 parser.add_argument('--optimizer', type=str, default="adam",
                     help='Optimizer (default: Adam')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input training batch-size')
 parser.add_argument('--epochs', type=int, default=25, metavar='N',
                     help='number of training epochs')
 
@@ -68,13 +75,7 @@ args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 use_visdom = args.visdom_url is not None
 
-
-# Handle randomization
-
-
 # Enable CUDA, set tensor type and device
-# todo: refractor this
-
 if args.cuda:
     dtype = torch.cuda.FloatTensor
     device = torch.device("cuda:0")
@@ -83,7 +84,7 @@ else:
     dtype = torch.FloatTensor
     device = torch.device("cpu")
 
-
+# Handle randomization
 if args.seed is not None:
     print('Seed: {}'.format(args.seed))
     np.random.seed(args.seed)
@@ -92,46 +93,37 @@ if args.seed is not None:
         torch.cuda.manual_seed(args.seed)
 
 
-def reconstruction_example(model, device, dtype):
+def reconstruction_example(model, data_loader):
+
     model.eval()
-    for _, (x, y) in enumerate(loader_val):
-        x = x.type(dtype)
-        x = x.to(device)
-        y = torch.from_numpy(one_hot_np(y, 10))
-        y = y.type(dtype)
-        y = y.to(device)
+    num_class = data_loader.num_class
+    img_shape = data_loader.img_shape[1:]
+
+    for _, (x, y) in enumerate(data_loader.test_loader):
+        y = one_hot(y, num_class)
+        y = y.type(type_tfloat(args.cuda))
         x_hat, _, _ = model(x, y)
         break
 
-    x = x[:10].cpu().view(10 * 28, 28)
-    x_hat = x_hat[:10].cpu().view(10 * 28, 28)
-    comparison = torch.cat((x, x_hat), 1).view(10 * 28, 2 * 28)
+    x = x[:num_class].cpu().view(num_class * img_shape[0], img_shape[1])
+    x_hat = x_hat[:num_class].cpu().view(num_class * img_shape[0], img_shape[1])
+    comparison = torch.cat((x, x_hat), 1).view(num_class * img_shape[0], 2 * img_shape[1])
     return comparison
 
 
-def latentspace_example(model, device):
-    draw = torch.randn(10, 20, device=device)
-    label = torch.eye(10, 10, device=device)
-    sample = model.decode(draw, label).cpu().view(10, 1, 28, 28)
+def latentspace_example(model, latent_size, data_loader):
+    num_class = data_loader.num_class
+    img_shape = data_loader.img_shape[1:]
+
+    draw = torch.randn(num_class, latent_size, )
+    label = torch.eye(num_class, num_class, )
+    sample = model.decode(draw, label).cpu().view(num_class, 1, img_shape[0], img_shape[1])
     return sample
 
 
 # save checkpoint
 def save_checkpoint(state, filename):
     torch.save(state, filename)
-
-
-# Training and Validation Dataloaders
-kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-
-loader_train = DataLoader(
-    datasets.MNIST('data', train=True, download=True,
-                   transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-
-loader_val = DataLoader(
-    datasets.MNIST('data', train=False, transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
 
 
 def get_optimizer(model):
@@ -143,13 +135,12 @@ def get_optimizer(model):
     return optimizer
 
 
-def execute_graph(model, conditional, loader_train, loader_val,
-                  loss_fn, scheduler, use_visdom):
+def execute_graph(model, conditional, data_loader, loss_fn, scheduler, optimizer, use_visdom):
     # Training loss
-    t_loss = train_validate(model, loader_train, loss_bce_kld, scheduler, conditional, train=True)
+    t_loss = train_validate(model, data_loader, loss_bce_kld, optimizer, conditional, train=True)
 
     # Validation loss
-    v_loss = train_validate(model, loader_val, loss_bce_kld, scheduler, conditional, train=False)
+    v_loss = train_validate(model, data_loader, loss_bce_kld, optimizer, conditional, train=False)
 
     # Step the scheduler based on the validation loss
     scheduler.step(v_loss)
@@ -165,45 +156,47 @@ def execute_graph(model, conditional, loader_train, loader_val,
         vis.add_scalar('Validation loss', idtag='valid', y=v_loss, x=epoch)
 
         # Visdom: Show generated images
-        sample = latentspace_example(model, device)
+        sample = latentspace_example(model, latent_size, data_loader)
         sample = sample.detach().numpy()
         vis.add_image('Generated sample ' + str(epoch), 'generated', sample)
 
-        # Visdom: Show example reconstruction
-        comparison = reconstruction_example(model, device, dtype)
+        # Visdom: Show example reconstruction from the test set
+        comparison = reconstruction_example(model, data_loader)
         comparison = comparison.detach().numpy()
         vis.add_image('Reconstruction sample ' + str(epoch), 'recon', comparison)
 
     return v_loss
 
 
-def train_validate(model, loader_data, loss_fn, scheduler, conditional, train):
+def train_validate(model, data_loader, loss_fn, optimizer, conditional, train):
     model.train() if train else model.eval()
+    loader = data_loader.train_loader if train else data_loader.test_loader
+
     batch_loss = 0
-    batch_sz = len(loader_data.dataset)
-    for batch_idx, (x, y) in enumerate(loader_data):
-        x = x.to(device)
+    batch_size = data_loader.batch_size
+    num_class = data_loader.num_class
+
+    for batch_idx, (x, y) in enumerate(loader):
+        loss = 0
         if train:
             opt.zero_grad()
         if conditional:
-            # use new function
-            y = torch.from_numpy(one_hot_np(y, 10))
-            # refractor out
-            y = y.type(dtype)
-            y = y.to(device)
+            y = one_hot(y, num_class)
+            y = y.type(type_tfloat(args.cuda))
+
             x_hat, mu, log_var = model(x, y)
         else:
             x_hat, mu, log_var = model(x)
 
         loss = loss_fn(x, x_hat, mu, log_var)
 
-        batch_loss += loss.item()
+        batch_loss += loss.item() / batch_size
 
         if train:
             loss.backward()
-            opt.step()
+            optimizer.step()
     # collect better stats
-    return batch_loss / batch_sz
+    return batch_loss / (batch_idx + 1)
 
 
 """
@@ -213,13 +206,25 @@ if use_visdom:
     vis = VisdomGrapher(args.uid, args.visdom_url, args.visdom_port)
 
 """
-Run a conditional one-hot VAE
+Get the dataloader
 """
-model = CVAE().type(dtype)
+data_loader = Loader(args.dataset_name, args.data_dir, args.download_data, True, args.batch_size, None, None, args.cuda)
+
+
+"""
+Run a conditional one-hot VAE, pass latent_dim, input_shape and num_class
+"""
+input_shape = data_loader.img_shape
+num_class = data_loader.num_class
+encoder_size = args.encoder_size
+latent_size = args.latent_size
+
+model = CVAE(input_shape, encoder_size, latent_size, num_class).type(dtype)
 model.apply(init_weights)
+
 opt = get_optimizer(model)
 scheduler = ReduceLROnPlateau(opt, 'min', verbose=True)
-early_stopping = EarlyStopping('min', 0.0005, 5)
+early_stopping = EarlyStopping('min', 0.0005, 15)
 
 num_epochs = args.epochs
 conditional = True
@@ -227,8 +232,8 @@ best_loss = np.inf
 # Main training and validation loop
 
 for epoch in range(1, num_epochs + 1):
-    v_loss = execute_graph(model, conditional, loader_train, loader_val,
-                           loss_bce_kld, scheduler, use_visdom)
+    v_loss = execute_graph(model, conditional, data_loader,
+                           loss_bce_kld, scheduler, opt, use_visdom)
 
     stop = early_stopping.step(v_loss)
 
@@ -246,9 +251,9 @@ for epoch in range(1, num_epochs + 1):
         break
 
 # Write a final sample to disk
-sample = latentspace_example(model, device)
+sample = latentspace_example(model, latent_size, data_loader)
 save_image(sample, 'output/sample_' + str(num_epochs) + '.png')
 
-# Make a final reconstrunction, and write to disk
-comparison = reconstruction_example(model, device, dtype)
+# Make a final reconstruction, and write to disk
+comparison = reconstruction_example(model, data_loader)
 save_image(comparison, 'output/comparison_' + str(num_epochs) + '.png')
--- vae_models.py
+++ vae_models.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import numpy as np
 
 
 class VAE(nn.Module):
@@ -14,14 +15,17 @@ class VAE(nn.Module):
     Simple VAE model
     """
 
-    def __init__(self):
+    def __init__(self, input_shape, encoder_size, latent_size):
         super(VAE, self).__init__()
+        self.input_shape = np.prod(list(input_shape))
+        self.encoder_size = encoder_size
+        self.latent_size = latent_size
         # tensor defs here
-        self.fc1 = nn.Linear(784, 400)
-        self.fc2_1 = nn.Linear(400, 20)
-        self.fc2_2 = nn.Linear(400, 20)
-        self.fc3 = nn.Linear(20, 400)
-        self.fc4 = nn.Linear(400, 784)
+        self.fc1 = nn.Linear(self.input_shape, self.encoder_size)
+        self.fc2_1 = nn.Linear(self.encoder_size, self.latent_size)
+        self.fc2_2 = nn.Linear(self.encoder_size, self.latent_size)
+        self.fc3 = nn.Linear(self.latent_size, self.encoder_size)
+        self.fc4 = nn.Linear(self.encoder_size, self.input_shape)
 
     def encode(self, x):
         h1 = F.relu(self.fc1(x))
@@ -44,7 +48,7 @@ class VAE(nn.Module):
         return output
 
     def forward(self, x):
-        mu, log_var = self.encode(x.view(-1, 784))
+        mu, log_var = self.encode(x.view(-1, self.input_shape))
         z = self.reparameterize(mu, log_var)
         return self.decode(z), mu, log_var
 
@@ -54,14 +58,19 @@ class CVAE(nn.Module):
     Simple Conditional VAE
     """
 
-    def __init__(self):
+    def __init__(self, input_shape, encoder_size, latent_size, num_class):
         super(CVAE, self).__init__()
+        self.input_shape = np.prod(list(input_shape))
+        self.encoder_size = encoder_size
+        self.latent_size = latent_size
+        self.num_class = num_class
+
         # tensor defs
-        self.fc1 = nn.Linear(784 + 10, 400)
-        self.fc2_1 = nn.Linear(400, 20)
-        self.fc2_2 = nn.Linear(400, 20)
-        self.fc3 = nn.Linear(20 + 10, 400)
-        self.fc4 = nn.Linear(400, 784)
+        self.fc1 = nn.Linear(self.input_shape + self.num_class, self.encoder_size)
+        self.fc2_1 = nn.Linear(self.encoder_size, self.latent_size)
+        self.fc2_2 = nn.Linear(self.encoder_size, self.latent_size)
+        self.fc3 = nn.Linear(self.latent_size + self.num_class, self.encoder_size)
+        self.fc4 = nn.Linear(self.encoder_size, self.input_shape)
 
     def encode(self, x, y):
         x_cond = torch.cat((x, y), 1)
@@ -86,48 +95,6 @@ class CVAE(nn.Module):
         return output
 
     def forward(self, x, y):
-        mu, log_var = self.encode(x.view(-1, 784), y)
-        z = self.reparameterize(mu, log_var)
-        return self.decode(z, y), mu, log_var
-
-
-class CVAE_C10(nn.Module):
-    """
-    Simple Conditional VAE for CIFAR10
-    """
-
-    def __init__(self):
-        super(CVAE, self).__init__()
-        # tensor defs
-        self.fc1 = nn.Linear(784 + 10, 400)
-        self.fc2_1 = nn.Linear(400, 20)
-        self.fc2_2 = nn.Linear(400, 20)
-        self.fc3 = nn.Linear(20 + 10, 400)
-        self.fc4 = nn.Linear(400, 784)
-
-    def encode(self, x, y):
-        x_cond = torch.cat((x, y), 1)
-        h1 = F.relu(self.fc1(x_cond))
-        mu = self.fc2_1(h1)
-        log_var = self.fc2_2(h1)
-        return mu, log_var
-
-    def reparameterize(self, mu, log_var):
-        if self.training:
-            std = torch.exp(0.5 * log_var)
-            eps = torch.randn_like(std)
-            draw = eps.mul(std).add_(mu)
-            return draw
-        else:
-            return mu
-
-    def decode(self, z, y):
-        z_cond = torch.cat((z, y), 1)
-        h3 = F.relu(self.fc3(z_cond))
-        output = torch.sigmoid(self.fc4(h3))
-        return output
-
-    def forward(self, x, y):
-        mu, log_var = self.encode(x.view(-1, 784), y)
+        mu, log_var = self.encode(x.view(-1, self.input_shape), y)
         z = self.reparameterize(mu, log_var)
         return self.decode(z, y), mu, log_var
